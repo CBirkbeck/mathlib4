4b4d786c6a562a232f5e4a6b95be5d4ecf0bf893
Etienne Marion <66847262+EtienneC30@users.noreply.github.com>
2026-09-07 10:31:33 +0000

    feat: define càdlàg functions (#43352)
    Introduce `IsRightContinuous`/`IsLeftContinuous` and `IsCadlag`/`IsCaglad` predicates.

--- Mathlib.lean
+++ Mathlib.lean
@@ -8316,6 +8316,7 @@ public import Mathlib.Topology.Order
 public import Mathlib.Topology.Order.AtTopBotIxx
 public import Mathlib.Topology.Order.Basic
 public import Mathlib.Topology.Order.Bornology
+public import Mathlib.Topology.Order.Cadlag
 public import Mathlib.Topology.Order.Category.AlexDisc
 public import Mathlib.Topology.Order.Category.FrameAdjunction
 public import Mathlib.Topology.Order.Compact
